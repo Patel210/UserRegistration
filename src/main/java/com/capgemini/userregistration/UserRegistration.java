5ced6b3d437c15dd950875f1a34e8e3730f4877b
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 15:16:59 +0530

    Added: UC1 to validate user first name

--- src/main/java/com/capgemini/userregistration/UserRegistration.java
+++ src/main/java/com/capgemini/userregistration/UserRegistration.java
@@ -1,7 +1,32 @@
 package com.capgemini.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-	 public static void main(String[] args) {
-		System.out.println("Hello World");
+
+	/**
+	 * To validate first name
+	 */
+	public void validateFirstName(String firstName) {
+
+		String regex = "(^[A-Z][a-z]{2,})";
+		Pattern r = Pattern.compile(regex);
+		Matcher m = r.matcher(firstName);
+		if (m.find()) {
+			System.out.println("First Name is Valid");
+		} else {
+			System.out.println("Invalid First Name!");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		UserRegistration userRegistration = new UserRegistration();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your first name to validate: ");
+		userRegistration.validateFirstName(sc.next());
+		sc.close();
 	}
 }
